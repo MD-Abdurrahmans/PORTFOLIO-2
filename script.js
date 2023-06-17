c799c660c393bc88de86af3e956777da35d7aec8
unknown <abraaz763@gmail.com>
2023-06-17 12:12:36 -0700

    helo

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+ScrollReveal({
+    //  reset: true,
+     distance:'50px',
+     duration:2000,
+     delay:200
+    });
+
+
+    ScrollReveal().reveal('.intros h1,.intros p', { origin:'left' });
+    ScrollReveal().reveal('.heading ', { origin:'top' });
+    ScrollReveal().reveal('.info-box ', { origin:'left' });
+    ScrollReveal().reveal('.form-box ', { origin:'top' });
+    ScrollReveal().reveal('.portfolio-box ', { origin:'left' });
+    ScrollReveal().reveal('.accordion-item ', { origin:'right' });
+    ScrollReveal().reveal('.carousel-inner ', { origin:'bottom' });
+    ScrollReveal().reveal('.marketing ', { origin:'left' });
+    ScrollReveal().reveal('.web ', { origin:'right' });
+    ScrollReveal().reveal('.hosting ', { origin:'left' });
+    ScrollReveal().reveal('.s-1 ', { origin:'left' });
+    ScrollReveal().reveal('.s-2 ', { origin:'right' });
